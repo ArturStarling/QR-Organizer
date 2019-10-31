be0d79a58e1a2d78552449c3f217b672b76ff787
ArturStarling <54860350+ArturStarling@users.noreply.github.com>
2019-10-31 16:51:16 -0300

    Update kivydatabase.py

--- kivydatabase.py
+++ kivydatabase.py
@@ -1,11 +1,11 @@
 from kivy.storage.jsonstore import JsonStore
-
+import cv2
 store = JsonStore('hello.json')
 
 # put some values
 store.put('tito', name='Mathieu', org='kivy', age=30, height=1.80, estate='São Paulo', country='Brasil')
 store.put('tshirtman', name='Gabriel', age=27)
-store.put('resistor', armario='armário 1', gaveta='gaveta 4', lugar='do lado da caixa laranja')
+store.put('resistor', armario='armário 1', gaveta='gaveta 4', lugar='do lado da caixa laranja', imagem='miseravel.jpg')
 
 # using the same index key erases all previously added key-value pairs
 #store.put('tito', name='Adriel', age=10)
@@ -18,7 +18,10 @@ store.put('resistor', armario='armário 1', gaveta='gaveta 4', lugar='do lado da
 #print('tito is', store.get('tito')['estate'])
 #print('tito is', store.get('tito')['country'])
 print('o objeto está no', store.get('resistor')['armario'], store.get('resistor')['gaveta'], store.get('resistor')['lugar'])
-
+image = cv2.imread(store.get('resistor')['imagem'])
+cv2.imshow('image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 # or guess the key/entry for a part of the key
 #for item in store.find(name='Gabriel'):
     #print('tshirtmans index key is', item[0])
