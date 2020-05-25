2015ae722e40fe8a0b315aa48617d53df5d9ab23
Adribom <50799373+Adribom@users.noreply.github.com>
2020-05-25 16:58:23 -0300

    Update main.py

--- testi/main.py
+++ testi/main.py
@@ -11,90 +11,95 @@ from kivy.uix.modalview import ModalView
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.image import Image
-
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 
-kv_text = '''
-<GameArea>:
+componentes = {'0':'Madeira1.jpg', '1':'Madeira1.jpg', '2':'Madeira2.jpg', '3':'Madeira3.jpg', '4':'Madeira4.jpg', '5':'Prateleira1.jpg'}
 
+
+Builder.load_string("""
+<MyImage@Image>: 
+    keep_ratio: False
+    allow_stretch: True
+<MenuScreen>:
     Button:
         id: flybox
         halign: 'center'
+        background_normal: 'botao.png'
+        background_down: 'kivy.png'
         border: 0, 0, 0, 0
         size_hint_y: 1/7
         size_hint_x: 1/8
         pos_hint: {'center_x': 1/2, 'center_y':1/10}
         on_press: app.qrcode()
-
-    Label:
-        halign: 'center'
-        pos_hint: {'center_x': 0.5, 'center_y': 0.185}
-        text: "QR CODE READER"
-        color: 1, .5, 0, 1
-
     FloatLayout:
         orientation: 'vertical'
-
-        TextInput:
-            on_text: app.process()
-            focus: True
-            halign: 'center'
-            id: dado
+        Button:
             size_hint_y: 1/14
             size_hint_x: .3
             pos_hint: {'center_x': 1/2, 'center_y':3/4}
-            name: 'test'
-            hint_text: 'Digite o objeto a ser buscado'
-            multiline: False
+            text: 'Menu de itens'
+            on_press:
+                root.manager.transition.direction = 'left'
+                root.manager.current = 'settings'
             
             background_color: 1, .6, 0, .25
-'''
-
-
-store = JsonStore('hello.json')
-store.put('resistores', local = 'arduino', qr = 2)
-store.put('arduino', local = 'arduino', qr = 3)
-
-
-storing = JsonStore('hellow.json')
-storing.put('1', local = 'pregos/parafusos/rebites', qr = 1)
-storing.put('2', local = 'arduino', qr = 2)
-storing.put('3', local = 'arduino', qr = 3)
-storing.put('4', local = 'componentes 1', qr = 4)
-storing.put('5', local = 'componentes 2', qr = 5)
-storing.put('6', local = 'armario madeira - 1ª prateleira', qr = 6)
-storing.put('7', local = 'armario madeira - 2ª prateleira', qr = 7)
-storing.put('8', local = 'armario madeira - 4ª prateleira', qr = 8)
-storing.put('9', local = 'prateleira canto - 3º', qr = 9)
-storing.put('10', local = 'prateleira canto - 4º', qr = 10)
-storing.put('11', local = 'prateleira canto - 5º', qr = 11)
-storing.put('12', local = 'armário porta - 1º', qr = 12)
-storing.put('13', local = 'armário porta - 2º', qr = 13)
-storing.put('14', local = 'armário porta - 3º', qr = 14)
-storing.put('15', local = 'armário porta - 4º', qr = 15)
-storing.put('32', local = 'armário porta - 5º', qr = 32)
-
+            on_press: root.manager.current = 'settings'
+<SettingsScreen>: 
+    Accor:
+        orientation: 'vertical'
+        AccordionItem: 
+            title: 'Madeira - 1º'
+            MyImage: 
+                source: 'Madeira1.jpg'
+        AccordionItem: 
+            title: 'Madeira - 2º'
+            MyImage: 
+                source: 'Madeira2.jpg'
+        AccordionItem: 
+            title: 'Madeira - 3º'
+            MyImage: 
+                source: 'Madeira3.jpg'
+        AccordionItem: 
+            title: 'Madeira - 4º'
+            MyImage: 
+                source: 'Madeira4.jpg'
+        AccordionItem: 
+            title: 'Prateleira - 1º'
+            MyImage: 
+                source: 'Prateleira1.jpg'
+""")
 
 def poppingup(n):
     view = ModalView(auto_dismiss=True, size_hint=(0.6, 0.2))
     view.add_widget(Label(text= n))
-    view.open()   
+    view.open()  
 
-class GameArea(FloatLayout):
+def poppando(x):
+    pop = Popup(title='test', content=Image(source=x), size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+class Accor(Accordion): 
+    pass
+
+class MenuScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
     pass
 
 
-class FlyApp(App):
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+
+class TestApp(App):
+
     def build(self):
-        Builder.load_string(kv_text)
-        return GameArea()
-
-    def process(self):
-        text = self.root.ids.dado.text
-
-        if text in store:
-            poppingup(store.get(text)['local'])
+        return sm
 
     def qrcode(self, *args):
             self.img1 = Image()
@@ -103,11 +108,14 @@ class FlyApp(App):
             qrDecoder = cv2.QRCodeDetector()
             data,bbox,rectifiedImage = qrDecoder.detectAndDecode(frame)
 
-            if data in storing:
-                poppingup(storing.get(data)['local'])
+            if data in componentes:
+                print(componentes[data])
+                poppando(componentes[data])
 
             else:
-                poppingup("qrcode inválido, tire outra foto")
+                poppingup("qrcode inválido, tire outra foto")  
+                
+
 
 if __name__ == '__main__':
-    FlyApp().run()
+    TestApp().run()
